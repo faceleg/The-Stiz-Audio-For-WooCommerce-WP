6a35885c2b6b532db7246903b97a99f4be4fc705
Michael Robinson <mike@pagesofinterest.net>
2012-06-21 22:28:23 +1200

    Initial commit

---
+++ include.php
@@ -0,0 +1,7 @@
+<?php
+
+include_once WCJD_ROOT.'/classes/WCJDStates.php';
+include_once WCJD_ROOT.'/classes/WCJDAdmin.php';
+include_once WCJD_ROOT.'/classes/WCJDMessages.php';
+include_once WCJD_ROOT.'/classes/WCJDInitialiser.php';
+
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: WC Jive Dig Audio Preview
+Plugin URI: http://pagesofinterest.net/projects/wc-jivedig-audio-preview/
+Description:
+Version: 1.0.0
+Author: Michael Robinson
+Author URI: http://pagesofinterest.net/
+License: http://www.gnu.org/licenses/gpl.html
+*/
+
+define('WCJD_ROOT', dirname(__FILE__));
+include_once WCJD_ROOT.'/include.php';
+
+$initialiser = new WCJDInitialiser();

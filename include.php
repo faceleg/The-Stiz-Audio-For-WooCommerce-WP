7a233d30e861dbaa98e6c75ff340243562bf6224
Michael Robinson <mike@pagesofinterest.net>
2012-06-21 23:52:54 +1200

    Updated include

--- include.php
+++ include.php
@@ -2,6 +2,7 @@
 
 include_once WCJD_ROOT.'/classes/WCJDStates.php';
 include_once WCJD_ROOT.'/classes/WCJDAdmin.php';
+include_once WCJD_ROOT.'/classes/WCJDProduct.php';
 include_once WCJD_ROOT.'/classes/WCJDMessages.php';
 include_once WCJD_ROOT.'/classes/WCJDInitialiser.php';
 

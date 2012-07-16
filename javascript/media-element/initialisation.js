b55838206b80829783d3c7f48d52ffbcf19dafe7
Michael Robinson <mike@pagesofinterest.net>
2012-07-16 21:38:34 +1200

    Minor stability fix.

--- javascript/media-element/initialisation.js
+++ javascript/media-element/initialisation.js
@@ -1,3 +1,3 @@
-jQuery('audio.wcjd-audio-preview').mediaelementplayer(jQuery.extend({}, {
-    // pauseOtherPlayers: true
-}, wcjdAudioPreviewOptions));
+(function($, options) {
+	if ($('audio.wcjd-audio-preview').length) $('audio.wcjd-audio-preview').mediaelementplayer(options);
+})(jQuery, wcjdAudioPreviewOptions);
